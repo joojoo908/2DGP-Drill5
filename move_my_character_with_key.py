39b0697ac6add4edf37942a9ed9710c457f2cb9e
joojoo908 <looloo8910@gmail.com>
2024-09-30 13:43:44 +0900

    방향키 입력따라 애니메이션 변경

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -1,6 +1,10 @@
 from pico2d import *
 import math
 
+move=1;
+x=300
+y=400
+
 open_canvas()
 
 back = load_image('TUK_GROUND.png')
@@ -15,15 +19,37 @@ def move_all(frame,move,x,y, rad):
     update_canvas()
     delay(0.05)
     
-def run_forward(s,e):
-    frame=0
-    for x in range(s,e,10):
-        move_all(frame,3,x,90,0)
-        frame = (frame+1)%4
+def run(frame,direction,x,y):
+    move_all(frame,direction,x,y,0)
 
+def handle_events():
+    global move
+    events = get_events()
 
+    for event in events:
+        if event.type == SDL_QUIT:
+            move=5
+        elif event.type == SDL_KEYDOWN and event.key==SDLK_RIGHT:
+            move=0
+        elif event.type == SDL_KEYDOWN and event.key==SDLK_LEFT:
+            move=1
+        elif event.type == SDL_KEYDOWN and event.key==SDLK_UP:
+            move=2
+        elif event.type == SDL_KEYDOWN and event.key==SDLK_DOWN:
+            move=3
 
-run_forward(0,800)
+frame=0
+while 1:
+    handle_events()
+    
+
+    if move==5:
+        break
+    else:
+       run(frame,move,x,y)
+       
+    frame = (frame+1)%4
+    #print(move)
 
     
 close_canvas()
